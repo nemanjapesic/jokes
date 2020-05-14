d0a156e3b9766a23c572189b3fcf8224914c254f
Nemanja Pesic <nemanja992@gmail.com>
2020-05-14 20:20:28 +0200

    Updates

---
+++ precache-manifest.72263870a99fcd4ea751ce31b05fb210.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afa83d0a4f39a6516c9bd76dd348e0e",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "e284f1490f1a0f6f99dc",
+    "url": "/jokes/static/js/2.087e94bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jokes/static/js/2.087e94bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e6b4f96d850335215d",
+    "url": "/jokes/static/js/main.5e2fbacf.chunk.js"
+  },
+  {
+    "revision": "545e81567633e2a74b37",
+    "url": "/jokes/static/js/runtime-main.d2551778.js"
+  }
+]);
